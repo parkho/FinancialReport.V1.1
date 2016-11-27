3077fbacc9e024c59d212eac5b320c452d929d50
Parviz Khorrami <pkhorraminejad@ymail.com>
2016-11-27 20:21:34 +0330

    Version Check Is Needed

--- core/module/Pfinance/Pfinance.php
+++ core/module/Pfinance/Pfinance.php
@@ -6,8 +6,16 @@ class Pfinance extends CodonModule
 		
 	public function index() 
     {
-		
-		$this->show('/pfinance/pilot_finance.php');
+		$revision = trim(file_get_contents(CORE_PATH.'/version'));
+			if($revision != 'simpilot 5.5.2')
+				{
+					echo '<center>phpVMS Version Installed Is Not Compatible With This Module!</center><br />';
+					echo '<center>phpVMS Version Installed: '.$revision.'</center>';
+				}
+			else
+			{
+				$this->show('/pfinance/pilot_finance.php');
+			}
  	}
 	
 	public function pilots()
